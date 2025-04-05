5be7b2ab972a897e956cbae4bd2be6ea50e1f6e3
Kaiser <ricoreven@gmail.com>
2025-04-05 17:10:39 +0600

    Filtering role

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,18 +1,25 @@
 import { IUser } from "./user.interface";
 import User from "./user.model";
 
+
+
+// Post Users Data
 export const createUserToDB = async (payload: IUser): Promise<IUser> => {
     const userInfo = await new User(payload);
     await userInfo.save();
     return userInfo;
 };
 
+
+// All Users Data
 export const getUsersFromDB = async () => {
     const users = await User.find();
     return users;
 };
 
+
+// findOne and Filter
 export const getUserByIdFromDB = async (payload: number): Promise<IUser | null> => {
-    const user = await User.findOne({ id: payload });
+    const user = await User.findOne({ id: payload }, { role: 1 });
     return user;
 };
